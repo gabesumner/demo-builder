678d0df4043c764d324ad531d8e8606c84c11e78
gsumner@salesforce.com <gabesumner@gmail.com>
2026-02-17 19:25:14 -0800

    Added password

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SITE_PASSWORD = process.env.SITE_PASSWORD;
+
+// HTTP Basic Auth
+if (SITE_PASSWORD) {
+  app.use((req, res, next) => {
+    const auth = req.headers.authorization;
+    if (auth) {
+      const [, encoded] = auth.split(" ");
+      const [, password] = Buffer.from(encoded, "base64").toString().split(":");
+      if (password === SITE_PASSWORD) return next();
+    }
+    res.set("WWW-Authenticate", 'Basic realm="Protected"');
+    res.status(401).send("Unauthorized");
+  });
+}
 
 app.use(express.static(join(__dirname, "dist")));
 
